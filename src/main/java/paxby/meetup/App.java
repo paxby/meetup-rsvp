9c5ddd26621b5b73033f45ddd0f2bf2600d8a669
Petter Axby <pkaxby@gmail.com>
2017-10-15 10:32:47 +0100

    Cleaned up app class

--- src/main/java/paxby/meetup/App.java
+++ src/main/java/paxby/meetup/App.java
@@ -1,70 +1,32 @@
 package paxby.meetup;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import paxby.meetup.model.Event;
-import paxby.meetup.model.Member;
-import paxby.meetup.model.Rsvp;
-import paxby.meetup.service.EventService;
+import paxby.meetup.service.RsvpService;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 @SpringBootApplication
 @Configuration
 public class App implements CommandLineRunner {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
-
-    private final EventService eventService;
-
-    @Value("${meetup.groups}")
-    private String groupNames;
+    private final RsvpService rsvpService;
 
     @Autowired
-    public App(EventService eventService) {
-        this.eventService = eventService;
+    public App(RsvpService rsvpService) {
+        this.rsvpService = rsvpService;
     }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
-    public static void lambdaRun() {
-        main(new String[0]);
-    }
-
     @Override
     public void run(String... args) {
-        List<String> groupNames = Arrays.asList(this.groupNames.split(","));
-        rsvpAnyUpcomingEvents(groupNames);
-    }
-
-    private void rsvpAnyUpcomingEvents(List<String> groupNames) {
-        Member self = eventService.getMember();
-
-        for (String groupName : groupNames) {
-            List<Event> events = eventService.getUpcomingEvents(groupName);
-            LOGGER.info("Group {} has {} upcoming events", groupName, events.size());
-
-            for (Event event : events) {
-                List<Rsvp> rsvps = eventService.getRsvps(event);
-                Optional<Rsvp> selfRsvp = eventService.getRsvp(rsvps, self);
-                LOGGER.info("Event: {} (rsvp: {})", event, selfRsvp.map(r -> r.getResponse().name()) .orElse("NONE"));
-
-                if (!selfRsvp.isPresent()) {
-                    LOGGER.info("Sending rsvp for {} event {}", groupName, event);
-                    eventService.rsvp(event, self);
-                }
-            }
-        }
+        rsvpService.rsvpAnyUpcomingEvents(Arrays.asList(args)); // TODO: Proper command line parsing
     }
 }
 
